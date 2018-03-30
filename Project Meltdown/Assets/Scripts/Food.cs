4a7e99ce1ec56fa829c78aa0fc468bf3dd944892
Unknown <Liannekisters@outlook.com>
2018-03-31 01:12:07 +0200

    Added new songs and made a food/rations script
    Basis for the food script has been laid out, the test scene has been updated.

---
+++ Project Meltdown/Assets/Scripts/Food.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+
+    
+    string foodName;
+    string description;
+
+    float nutritionalValue;
+
+    int expirationDate;
+    bool isSpoiled;
+    int radiationLevels;
+    long rationsAmount;
+
+    
+
+}
