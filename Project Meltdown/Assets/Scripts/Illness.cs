ea167ba9ba09607ab4ad509a82346bc625ff4e0a
Lianne Kisters <Liannekisters@outlook.com>
2018-04-13 01:13:28 +0200

    Comitted new changes
    CTS

---
+++ Project Meltdown/Assets/Scripts/Illness.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Illness : MonoBehaviour {
+
+    string diseaseName;
+    string diseaseDescription;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
